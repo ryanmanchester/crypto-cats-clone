adce0f73e2fd4c462442ccb099aadb6f00034a7a
Ryan Manchester <ryanm17@gmail.com>
2022-01-05 15:22:30 -0500

    completes test suite for smart contract

--- test/Color.test.js
+++ test/Color.test.js
@@ -30,11 +30,34 @@ contract('Color', (accounts) => {
     it('creates new token', async () => {
       const result = await contract.mint('#EC058E')
       const totalSupply = await contract.totalSupply()
+      //Success
       assert.equal(totalSupply, 1)
       const event = (result.logs[0].args)
       assert.equal(event.tokenId.toNumber(), 1, 'correct id')
       assert.equal(event.from, '0x0000000000000000000000000000000000000000', 'from is correct')
       assert.equal(event.to, accounts[0], 'to is correct')
+      //Failure: cannot mint same color twice
+      await contract.mint('#EC058E').should.be.rejected
+    })
+  })
+
+  describe('indexing', async () => {
+    it('lists colors', async () => {
+      //Mint 3 tokens
+      await contract.mint('#5386E4')
+      await contract.mint('#FFFFFF')
+      await contract.mint('#000000')
+      const totalSupply = await contract.totalSupply()
+
+      let color
+      let result = []
+      for (let i = 1; i <= totalSupply; i++) {
+        color = await contract.colors(i - 1)
+        result.push(color)
+      }
+      let expected = ['#EC058E','#5386E4', '#FFFFFF', '#000000']
+      assert.equal(result.join(','), expected.join(','))
+
     })
   })
 })
