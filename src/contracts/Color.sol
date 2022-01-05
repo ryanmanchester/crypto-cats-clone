352980813fc97dae6e1f871e8c62aa02f0d6e065
Ryan Manchester <ryanm17@gmail.com>
2022-01-05 03:32:11 -0500

    migrates color attribute to abi

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyNFT is ERC721 {
+contract Color is ERC721 {
     constructor() ERC721 ("Color", "CLR") public {
     }
 }
