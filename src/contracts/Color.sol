a483c9e8624bb7e9e2ec47f102644a84c1c20af6
Ryan Manchester <ryanm17@gmail.com>
2022-01-05 14:47:19 -0500

    passes minting test

--- src/contracts/Color.sol
+++ src/contracts/Color.sol
@@ -1,19 +1,40 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Color is ERC721 {
+contract Color is ERC721, ERC721Enumerable {
   string[] public colors;
   mapping(string => bool) _colorExists;
-    constructor() ERC721 ("Color", "CLR") public {
+    constructor() ERC721("Color", "CLR") public {
     }
+
+
     function mint(string memory _color) public {
       //Require unique color
-      uint _id = colors.push(_color);
+      colors.push(_color);
+      uint _id = colors.length - 1;
       _mint(msg.sender, _id);
       _colorExists[_color] = true;
       //Color - add
      //Call mint function
      //Color - track it
     }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+    internal
+    override(ERC721, ERC721Enumerable)
+{
+    super._beforeTokenTransfer(from, to, tokenId);
+}
+
+function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721, ERC721Enumerable)
+    returns (bool)
+{
+    return super.supportsInterface(interfaceId);
+}
+
 }
